2fd13e640c479a2d68db96f782361737be83f141
Abubakr3106001343 <engg.abubakr@gmail.com>
2024-08-31 13:03:04 +0500

    Add files via upload

---
+++ program 9.3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+void table(int n);
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"enter a number:";
+    cin>>num;
+    table(num);
+    return 0;
+}
+ void table(int n)
+ {
+ int c;
+ for(c=1;c<=10;c++)
+ {
+ cout<<n<<"*"<<c<<"="<<n*c<<endl;
+ }
+ }
